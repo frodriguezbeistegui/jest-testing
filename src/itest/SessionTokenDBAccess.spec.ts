89b53f289921104874c06bad4fc9db0af6ccba42
facundo <frodriguezbeistegui@gmail.com>
2023-02-04 00:29:32 -0300

    feat: first integration test

--- src/itest/SessionTokenDBAccess.spec.ts
+++ src/itest/SessionTokenDBAccess.spec.ts
@@ -21,7 +21,22 @@ describe('First itest suite', () => {
         await sessionTokenDBAccess.storeSessionToken(someSessionToken);
         const resultToken = await sessionTokenDBAccess.getToken(someSessionToken.tokenId);
 
-        expect(resultToken).toEqual(someSessionToken);
+        expect(resultToken).toMatchObject(someSessionToken);
 
-     })
+     });
+
+     it('delete sessionToken', async () => { 
+        await sessionTokenDBAccess.deleteToken(someSessionToken.tokenId);
+        const resultToken = await sessionTokenDBAccess.getToken(someSessionToken.tokenId)
+        expect(resultToken).toBeUndefined()
+      })
+
+      it('delete missing sessionToken throws error', async () => { 
+        try {
+            await sessionTokenDBAccess.deleteToken(someSessionToken.tokenId);
+        } catch (error) {
+            expect(error).toBeInstanceOf(Error)
+            expect(error).toHaveProperty('message', 'SessionToken not deleted!')
+        }
+      })
  })
